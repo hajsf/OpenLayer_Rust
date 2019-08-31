2317df0e2c957a4d8809e55e6bbd6a91ee4ec77a
hajsf <hasan.ajsf@gmail.com>
2019-08-31 11:56:36 +0300

    Fix routes calling

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ use rocket_contrib::{serve::StaticFiles, templates::Template};
 use std::{thread, sync::Mutex};
 
 mod socket; mod routes; mod models; mod databases;
-use crate::routes::submit_task::static_rocket_route_info_for_submit_task;
-use crate::routes::index::static_rocket_route_info_for_index;
+use crate::routes::{index, submit_task};
 use self::models::{client::Client, truck::Truck};
 use crate::databases::main_connection::PgDb1;
 
@@ -26,7 +25,7 @@ fn main() {
 
     rocket::ignite()
     .mount("/static", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
-    .mount("/", routes![index, submit_task])
+    .mount("/", routes![index::index, submit_task::submit_task])
     .attach(Template::fairing())
     .attach(PgDb1::fairing())
     .launch();
